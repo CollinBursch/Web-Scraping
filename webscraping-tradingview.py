4f7b54a8889cba05f7d32f9409740086cec673e2
Johnny Bhojwani <johnnyvb@gmail.com>
2022-03-29 23:50:34 -0500

    Initial Commit

---
+++ webscraping-tradingview.py
@@ -0,0 +1,35 @@
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+
+
+
+
+##############FOR MACS THAT HAVE ERRORS LOOK HERE################
+## https://timonweb.com/tutorials/fixing-certificate_verify_failed-error-when-trying-requests_html-out-on-mac/
+
+############## ALTERNATIVELY IF PASSWORD IS AN ISSUE FOR MAC USERS ########################
+##  > cd "/Applications/Python 3.6/"
+##  > sudo "./Install Certificates.command"
+
+
+url = 'https://www.tradingview.com/markets/stocks-usa/market-movers-gainers/'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+
+		
+
+
+
+
+
+
+#SOME USEFUL FUNCTIONS IN BEAUTIFULSOUP
+#-----------------------------------------------#
+# find(tag, attributes, recursive, text, keywords)
+# findAll(tag, attributes, recursive, text, limit, keywords)
+
+#Tags: find("h1","h2","h3", etc.)
+#Attributes: find("span", {"class":{"green","red"}})
+#Text: nameList = Objfind(text="the prince")
+#Limit = find with limit of 1
+#keyword: allText = Obj.find(id="title",class="text")
+
